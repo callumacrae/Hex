a01b61ad9b374c5ce1f1d677e820baf877dccb9f
Callum Macrae <callumacrae95@gmail.com>
2011-05-28 17:28:14 +0100

    Changed some stuff.

--- hex.js
+++ hex.js
@@ -1,52 +1,12 @@
 var IRC = require('./bot'),
 	config = require('./config'),
-	hex;
+	handle = require('./handler'),
+	hex = new IRC(config);
 
-hex = new IRC(config);
-
-function handle(info)
+function handler(info)
 {
-	var chan, cmd, cmd_end, index, nick, reply;
-	nick = info[1];
-	chan = info[2];
-
-	index = info[3].indexOf(' ');
-	cmd = (index === -1) ? info[3] : info[3].slice(0, index);
-	cmd_end = (index === -1) ? null : info[3].slice(index + 1);
-
-	switch (cmd.toLowerCase())
-	{
-		case 'a':
-		case 'admin':
-			index = cmd_end.indexOf(' ');
-			cmd = (index === -1) ? cmd_end : cmd_end.slice(0, index);
-			cmd_end = (index === -1) ? null : cmd_end.slice(index + 1);
-
-			switch (cmd.toLowerCase())
-			{
-				case 'help':
-					reply = 'GO DIE';
-					break;
-
-				default:
-					//command not found
-			}
-			break;
-
-		case 'help':
-			reply = 'Hello!';
-			break;
-
-		default:
-			//command not found or in a file
-			break;
-	}
-
-	if (reply)
-	{
-		hex.msg(chan, nick + ': ' + reply);
-	}
+	handle(info, hex);
 }
 
-hex.on(/^:([^!]+)![^@]+@[^ ]+ PRIVMSG (#[^ ]+) :hex: (.+)/i, handle);
-hex.on(/^:([^!]+)![^@]+@[^ ]+ PRIVMSG ([^# ]+) :(.+)/i, handle)
+hex.on(/^:([^!]+)![^@]+@[^ ]+ PRIVMSG (#[^ ]+) :hex: (.+)/i, handler);
+hex.on(/^:([^!]+)![^@]+@[^ ]+ PRIVMSG ([^# ]+) :(.+)/i, handler);
