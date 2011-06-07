7a4eb20639db2825d2f179fd4990c3926046e3a8
Callum Macrae <callumacrae95@gmail.com>
2011-06-07 19:44:58 +0100

    Written more on the handler... kinda.

--- games/index.js
+++ games/index.js
@@ -1,6 +1,6 @@
 var IRC = require('bot'),
 	config = require('../config'),
-	hex, handler, admins = {};
+	hex, handle, game = {};
 
 config.user = config.user_game;
 config.chans = ['#games'];
@@ -9,11 +9,33 @@ eval(fs.readFileSync('./handler.js', 'utf8'));
 
 hex = new IRC(config);
 
+game.players = [];
+game.answers = [];
+game.votes = {};
+game.game = {};
+
 hex.on(/^:([^!]+)![^@]+@[^ ]+ PRIVMSG ([^ ]+) :(.+)$/i, function(info)
 {
-	var re = handle(info);
+	var re = handle(info, info[1] === 'callumacrae');
 	if (re === 'FLUSH')
 	{
 		eval(fs.readFileSync('./handler.js', 'utf8'));
 	}
 });
+
+game = {
+	players: [['callumacrae', 12], ['test', 5]],
+	answers: [
+		['callumacrae', 'My answer here'],
+		['test', 'Fake answer']
+	],
+	votes: {
+		callumacrae: 1 //answer id
+	},
+	game: {
+		current: ['initials', 'WIFB'],
+		next: 'word', //event, word, law, initials, people
+		rounds: 2,
+		status: 'vote' //pre, submit, votw
+	}
+}
