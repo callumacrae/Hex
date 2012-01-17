7f20f5dfdae88538ecbd6d935a49d3d1d7dd1c13
Callum Macrae <callum@lynxphp.com>
2012-01-17 20:48:38 +0000

    ...made the last four commits work.

--- js_run.js
+++ js_run.js
@@ -1,2 +1,11 @@
 var vm = require('vm'), sandbox = {};
-console.log(vm.runInNewContext(process.argv[2], sandbox));
+
+var output = vm.runInNewContext(process.argv[2], sandbox);
+
+if (typeof output === 'object' && !(output instanceof Array)) {
+	output = JSON.stringify(output);
+} else if (typeof output === 'string') {
+	output = '"' + output + '"';
+}
+
+console.log(output);
