9837dd18fd5c422b4ffc0ef77467616f836e3138
GtoXic <rocky4126@hotmail.co.uk>
2012-02-02 07:45:20 +0000

    finished config

--- demo/config.php
+++ demo/config.php
@@ -1,3 +1,18 @@
-;Configuration
-server=
-port=
+<?php
+$config = array(
+	"server" => "irc.x10hosting.com:6667",
+	"channels" => "#x10bot,#x10bot-backend",
+	"nickserv" => "",
+	"serverpass" => "",
+);
+$config['irclog'] = array(
+	"file_location" => "./irc.log",
+	"format" => "plaintext",
+);
+$config['botlog'] = array(
+	"file_location" => "./bot.log",
+	"format" => "plaintext",
+	"channel_level" => "debug",
+	"memo_level" => "error",
+	"email_level" => "error",
+);
