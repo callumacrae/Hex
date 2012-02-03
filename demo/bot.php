b78f39314f6f947835abc67d11b287c59870a06d
GtoXic <rocky4126@hotmail.co.uk>
2012-02-03 18:05:39 +0000

    Added core functionality

--- demo/bot.php
+++ demo/bot.php
@@ -10,11 +10,23 @@
 			if(!$this->sock = fsockopen($serverdetails[0],$serverdetails[1])){
 				die($this->error("Failed to gain a connection"));
 			}
-			$this->raw();
+			$this->raw("NICK {$this->config['nick']}");
+			$this->raw("USER {$this->config['nick']} {$this->config['nick']} {$this->config['nick']} :{$this->config['nick']}");
 			$this->main();
 		}
 		function main(){
-			
+			$data = trim(fgets($this->sock));
+			if($data != ""){
+				$this->log($msg);
+			}
+			$ex = explode(" ",$data);
+			if($ex[1] == "001"){ //Checks for code sent by IRC saying that a connection has been made
+				$this->msg("NickServ","IDENTIFY {$this->config['nickserv']}");
+				$this->raw("JOIN {$this->config['chans']}");
+			}
+			if($ex[0] == "PING"){
+				$this->raw("PONG {$ex[1]}");
+			}s
 		}
 		function raw($msg){
 			
@@ -24,6 +36,9 @@
 		}
 		function log($msg){
 			
+		}
+		function msg($chan,$msg){
+			$this->raw("PRIVMSG $chan $msg");
 		}
 	}
 ?>
