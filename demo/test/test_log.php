9361492933f5edc8a6c35e05b17ace58e0968b5c
Xavier L <xavier.l@afrosoft.tk>
2012-02-03 18:39:42 -0500

    updated hgignore and bot/log .php

--- demo/test/test_log.php
+++ demo/test/test_log.php
@@ -2,9 +2,9 @@
 
 require './../log.php';
 
-$log = IRCBot_Log::getInstance();
+$log = IRCBot_Log::getInstance(null);
 
 var_dump($log);
 
-var_dump($log->debug("testing", "log", "debug", null));
+var_dump($log->debug("testing", "log", "debug", null, IRCBot_Log::TO_FILE | IRCBot_Log::TO_STDOUT));
 
