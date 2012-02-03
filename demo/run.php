ee192e1431d8f95e8c9cf6578319a3db0c4d63cc
GtoXic <rocky4126@hotmail.co.uk>
2012-02-03 20:10:29 +0000

    added error reporting

--- demo/run.php
+++ demo/run.php
@@ -1,4 +1,15 @@
 <?php
+ini_set("display_errors","on");
+function getarg($arg){
+	foreach($argv as $argu){
+		if($argu == $arg){
+			return true;
+		}
+	}
+	return false;
+}
+if(getarg("debug") == true){
+	error_reporting(E_ALL);
 
 require_once 'bot.php';
 
